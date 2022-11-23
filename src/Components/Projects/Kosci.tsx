fb216e2f1623eaf0523dd1e60230b542d029604a
Grzegorz Uberman <ubermangrzegorz@gmail.com>
2022-11-23 21:45:37 +0100

    dice-roller description

--- src/Components/Projects/Kosci.tsx
+++ src/Components/Projects/Kosci.tsx
@@ -1,9 +1,46 @@
-import { StyledContainer } from "../Styles/Project.styles"
+import { StyledContainer } from "../Styles/Project.styles";
+import screenshot from "../../images/kosci.jpg";
 
 export const Kosci = () => {
-    return (
-        <StyledContainer>
-            Dice
-        </StyledContainer>
-    )
-}
+  return (
+    <StyledContainer>
+      <a href="https://kosci-5cf52.web.app/" target="blank">
+        <img src={screenshot}></img>
+      </a>
+      <h1>Kości (Dice)</h1>
+      <p>Individual training project</p>
+      <p>
+        This one page app allows you to generate random numbers the same
+        way that rolling any of the popular polyhedral dice does.
+      </p>
+      <div className="miniheader">
+        Repository:{" "}
+        <a href="https://github.com/g-uberman/dice-roller" target="blank">
+          github.com/g-uberman/dice-roller
+        </a>
+      </div>
+      <div className="miniheader">
+        Deployed app:{" "}
+        <a href="https://kosci-5cf52.web.app/" target="blank">
+          kosci-5cf52.web.app
+        </a>
+      </div>
+
+      <br />
+      <div className="miniheader green">Technologies used: </div>
+      <ul>
+        <li>JavaScript</li>
+      </ul>
+      <br />
+      <div className="miniheader green">Key features: </div>
+      <ul>
+        <li>LocalStorage saving the last 10 roll results</li>
+        <li>
+          Toggleable exploding dice (rolling another die when the first one's
+          result was its maximum)
+        </li>
+        <li>Fully responsive</li>
+      </ul>
+    </StyledContainer>
+  );
+};
--- src/Components/Styles/Project.styles.tsx
+++ src/Components/Styles/Project.styles.tsx
@@ -2,18 +2,20 @@ import styled from "styled-components";
 
 export const StyledContainer = styled.main`
     height: 100%;
-    padding: 1em 4em;
+    padding: 2em 4em;
     background: #fffffff5;
     z-index: 1;
 
     img {
         width: 100%;
         margin-bottom: 1em;
-        transition: all 0.3s;
+        transition: all 0.2s;
+        filter:  grayscale(50%);
     }
 
     img:hover {
-        box-shadow: 0px 5px 10px #00000050;
+        box-shadow: 0px 3px 10px #00000050;
+        filter: none;
     }
 
     ul {
