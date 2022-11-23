f248081789ea35c2b972785db9a7eb9662847956
Grzegorz Uberman <ubermangrzegorz@gmail.com>
2022-11-23 17:53:00 +0100

    footer styled

--- src/Components/Styles/Footer.styles.tsx
+++ src/Components/Styles/Footer.styles.tsx
@@ -3,8 +3,8 @@ import styled from "styled-components";
 export const StyledFooter = styled.footer`
     text-align: center;
     font-size: 0.8em;
-    margin-bottom: 0.3em;
     background: #fff;
+    padding: 10px;
 
     a, a:visited {
     color: #000;
